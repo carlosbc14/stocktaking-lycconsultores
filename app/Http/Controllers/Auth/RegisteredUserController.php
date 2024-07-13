5112a5f7b26154246832c9505925de14f143b13b
CarlosBC14 <carlosbadillac@outlook.com>
2024-07-13 18:22:17 -0400

    Asignación de rol de propietario a usuario registrado

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -50,6 +50,8 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $user->assignRole('owner');
+
         event(new Registered($user));
 
         Auth::login($user);
