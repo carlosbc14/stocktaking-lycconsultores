96692c1bf5ede2feb7b1f45c037e348bc08ba739
CarlosBC14 <carlosbadillac@outlook.com>
2024-07-14 01:18:36 -0400

    Corrección del nombre del permiso

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -12,7 +12,7 @@ export default function Dashboard({ auth }) {
                 <div className="flex flex-1 flex-col text-center p-6 text-gray-900">
                     <h2 className="font-bold text-4xl">{__('¡Welcome to Stocktaking LYC Consultores!')}</h2>
                     {auth.user.company_id !== null ||
-                        !auth.user.permissions.some((per) => per.name === 'create company') || (
+                        !auth.user.permissions.some((per) => per.name === 'write company') || (
                             <>
                                 <p className="mt-10">{__('Create your company to start using Stocktaking')}</p>
                                 <Link href={route('company.create')}>
