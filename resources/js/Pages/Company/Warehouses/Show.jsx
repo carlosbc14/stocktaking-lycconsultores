60dea5bc623c1405f86be53a371c61bdde33e5ae
CarlosBC14 <carlosbadillac@outlook.com>
2024-07-15 22:43:04 -0400

    Corrección de nombre del permiso en el frontend para editar almacenes

--- resources/js/Pages/Company/Warehouses/Show.jsx
+++ resources/js/Pages/Company/Warehouses/Show.jsx
@@ -7,7 +7,7 @@ export default function Show({ auth, warehouse }) {
             <div className="space-y-6">
                 <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     <ShowWarehouseInformation
-                        canEdit={auth.user.permissions.some((per) => per.name === 'edit warehouse')}
+                        canEdit={auth.user.permissions.some((per) => per.name === 'edit warehouses')}
                         warehouse={warehouse}
                     />
                 </div>
